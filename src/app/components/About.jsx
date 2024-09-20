d91c08240fd8bd63170b5873eafaa5504cc9149b
MohammadMaasir <maasir554@gmail.com>
2024-09-20 16:59:55 +0530

    initialized about us section

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -1,7 +1,11 @@
 
 export const About = () => (
-    <section className="w-screen min-h-screen h-full">
-        
+    <section className="w-screen min-h-screen h-full bg-gradient-to-t from-purple-900 to-purple-950 flex justify-center items-center p-10">
+        <article className=" w-full max-w-6xl grid grid-cols-2 gap-5 content-center items-center">
+            <div className="bg-yellow-300 h-24 col-span-1"> </div>
+            <div className="bg-yellow-300 h-20 col-span-1"> </div>
+            <div className="bg-yellow-300 h-20 col-span-2"> </div>
+        </article>
     </section>
 )
 
--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -32,7 +32,7 @@ export const Hero = () => {
     },[])
     
     return(
-    <section id="hero-section" className="bg-[url('/herobg.png')] h-auto md:h-[200dvh] bg-top bg-portrait md:bg-landscape bg-no-repeat bg-fixed relative before:content-[''] before:h-full before:w-full before:bg-black before:flex before:-z-10 before:absolute before:top-0 ">
+    <section id="hero-section" className="bg-[url('/herobg.png')] h-auto md:h-[200lvh] bg-top bg-portrait sm:bg-landscape bg-no-repeat bg-fixed relative before:content-[''] before:h-full before:w-full before:bg-black before:flex before:-z-10 before:absolute before:top-0 ">
         
         {/* Our Mascot Robot */}
         <div id="robot-image" className=" max-w-[400px] md:max-w-[1000px] w-full sm:w-3/4 md:w-1/2 h-auto flex items-center justify-center fixed left-1/2 -translate-x-1/2 bottom-0 "
@@ -48,13 +48,15 @@ export const Hero = () => {
         </div>
  
         {/* Big Heading */}
-        <div className="w-full h-auto min-h-dvh flex items-center justify-center text-[18vw] md:text-[20vw] tracking-widest font-bold z-0">
-            <h1  className="text-headingcolor text-center mb-8">
+        <div className="w-full h-auto min-h-lvh flex items-center justify-center text-[18vw] md:text-[20vw] tracking-widest font-bold z-0">
+            <div className="overflow-hidden h-auto">
+                <h1 className="text-headingcolor text-center mb-8 leading-[17vw] z-0">
                 ROBOTICS
-            </h1>
+                </h1>
+            </div>
         </div>
         {/* Button Box */}
-        <div className="w-full h-hull min-h-[70dvh] md:h-dvh flex items-center justify-center p-10 ">
+        <div id="hero-register" className="w-full h-hull min-h-[70lvh] md:h-lvh flex items-center justify-center p-10 ">
             <div className="relative w-[90vw] md:w-[80vw] lg:w-[70vw] bg-gradient-to-b from-transparent to-white/30 backdrop-blur-sm  h-[200px] sm:h-[35vw] md:h-[22vw] lg:h-[18vw] rounded-xl md:rounded-3xl overflow-visible flex flex-row justify-end items-end z-20">
                 <span className="absolute bottom-[100px] sm:bottom:12 md:bottom-8 left-4 text-[15vw] md:text-[10vw] text-[#B373FF] leading-[12vw] sm:leading-[12vw] md:leading-[8vw] font-bold">
                 <h1>DIVE</h1>
